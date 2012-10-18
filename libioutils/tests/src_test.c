7e0829cb0d04eb7702fee77bec04bfb87bd15b85
Nicolas CARRIER <nicolas.carrier@parrot.com>
2012-10-18 15:06:03 +0200

    [DEV] Added duplex source init test

--- libioutils/tests/src_test.c
+++ libioutils/tests/src_test.c
@@ -23,17 +23,53 @@ static int my_dummy_callback(__attribute__((unused)) io_src_t *source)
 static void testSRC_INIT(void)
 {
 	int pipefd[2] = {-1, -1};
+	int fd;
 	io_src_t src;
 	int ret;
 
 	ret = pipe(pipefd);
 	CU_ASSERT_NOT_EQUAL_FATAL(ret, -1);
+	fd = open("/dev/urandom", O_RDWR | O_CLOEXEC);
+	CU_ASSERT_NOT_EQUAL_FATAL(fd, -1);
 
 	/* normal use case */
+	/* put garbage in the struct */
+	ret = read(fd, &src, sizeof(src));
+	CU_ASSERT_EQUAL(ret, sizeof(src));
 	ret = io_src_init(&src, pipefd[0], IO_IN, my_dummy_callback);
 	CU_ASSERT_EQUAL(ret, 0);
 	CU_ASSERT_EQUAL(src.fd, pipefd[0]);
 	CU_ASSERT_EQUAL(src.type, IO_IN);
+	CU_ASSERT_EQUAL(src.callback, my_dummy_callback);
+
+	CU_ASSERT_EQUAL(src.events, 0);
+
+	CU_ASSERT_EQUAL(src.active, 0);
+	CU_ASSERT_PTR_NULL(src.node.next);
+	CU_ASSERT_PTR_NULL(src.node.prev);
+
+	/* put garbage in the struct */
+	ret = read(fd, &src, sizeof(src));
+	CU_ASSERT_EQUAL(ret, sizeof(src));
+	ret = io_src_init(&src, pipefd[1], IO_OUT, my_dummy_callback);
+	CU_ASSERT_EQUAL(ret, 0);
+	CU_ASSERT_EQUAL(src.fd, pipefd[1]);
+	CU_ASSERT_EQUAL(src.type, IO_OUT);
+	CU_ASSERT_EQUAL(src.callback, my_dummy_callback);
+
+	CU_ASSERT_EQUAL(src.events, 0);
+
+	CU_ASSERT_EQUAL(src.active, 0);
+	CU_ASSERT_PTR_NULL(src.node.next);
+	CU_ASSERT_PTR_NULL(src.node.prev);
+
+	/* put garbage in the struct */
+	ret = read(fd, &src, sizeof(src));
+	CU_ASSERT_EQUAL(ret, sizeof(src));
+	ret = io_src_init(&src, fd, IO_DUPLEX, my_dummy_callback);
+	CU_ASSERT_EQUAL(ret, 0);
+	CU_ASSERT_EQUAL(src.fd, fd);
+	CU_ASSERT_EQUAL(src.type, IO_DUPLEX);
 	CU_ASSERT_EQUAL(src.callback, my_dummy_callback);
 
 	CU_ASSERT_EQUAL(src.events, 0);
@@ -53,6 +89,7 @@ static void testSRC_INIT(void)
 	/* cleanup */
 	close(pipefd[0]);
 	close(pipefd[1]);
+	close(fd);
 }
 
 static void testTO_SRC(void)
@@ -71,7 +108,6 @@ static const test_t tests[] = {
 				.fn = testTO_SRC,
 				.name = "to_src"
 		},
-
 
 		/* NULL guard */
 		{.fn = NULL, .name = NULL},
