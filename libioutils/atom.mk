ee53cd98a4ca6c591f23cef2ef847d43a4c392aa
Nicolas CARRIER <nicolas.carrier@parrot.com>
2012-11-26 13:53:52 +0100

    [BUILD] Added support for Alchemy build environment

---
+++ libioutils/atom.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+###############################################################################
+# libioutils
+###############################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libioutils
+
+LOCAL_EXPORT_C_INCLUDES  := $(LOCAL_PATH)/include
+
+LOCAL_SRC_FILES := $(call all-c-files-under,src,.c)
+
+LOCAL_LIBRARIES := librs
+
+ifdef LIBIOUTILS_FAUTES_SUPPORT
+LOCAL_SRC_FILES += $(call all-c-files-under,tests,.c)
+
+LOCAL_LIBRARIES += libfautes
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
