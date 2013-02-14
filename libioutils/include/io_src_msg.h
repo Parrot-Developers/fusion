4c240fdcff66a14257ee0dbf7e9192484bc21378
Nicolas CARRIER <nicolas.carrier@parrot.com>
2013-02-14 11:16:44 +0100

    [DEV][src_msg] Add write capability

--- libioutils/include/io_src_msg.h
+++ libioutils/include/io_src_msg.h
@@ -2,7 +2,7 @@
  * @file io_src_msg.h
  * @date 24 oct. 2012
  * @author nicolas.carrier@parrot.com
- * @brief Source for reading fixed length messages
+ * @brief Source for reading / writing fixed length messages
  *
  * Copyright (C) 2012 Parrot S.A.
  */
@@ -22,12 +22,19 @@ struct io_src_msg;
 
 /**
  * @typedef io_src_msg_cb_t
- * @brief Called when an entire message has been received
- * @param msg Message source
+ * @brief Called when an entire message has been received or when the source is
+ * ready to write a message. No partial message can be read or written.
+ * If the event type is IO_IN, the message received is stored in src->msg and
+ * can be used by the callback.
+ * If the event type is IO_OUT, the callback must set src->msg to point to the
+ * message to send, which will be sent just after the callback has been called.
+ * @param src Message source
+ * @param evt Type of the event to process, either IO_IN or IO_OUT, not
+ * IO_DUPLEX
  * @return errno compatible value, positive for only a warning, negative if the
  * source must be removed, 0 on success
  */
-typedef int (io_src_msg_cb_t)(struct io_src_msg *msg);
+typedef int (io_src_msg_cb_t)(struct io_src_msg *src, enum io_src_event evt);
 
 /**
  * @typedef io_src_msg
@@ -36,28 +43,32 @@ typedef int (io_src_msg_cb_t)(struct io_src_msg *msg);
 struct io_src_msg {
 	/** inner monitor source */
 	struct io_src src;
-	/** user callback, notified a message has been received */
+	/** user callback, notified when I/O is possible */
 	io_src_msg_cb_t *cb;
-	/** fixed-length of the messages to read */
+	/** fixed-length of the messages to read/write */
 	unsigned len;
 	/**
-	 * points to a user buffer able to receive a message, filled at each cb
-	 * call with the message just read
+	 * points to a user buffer able to contain a whole message, filled at
+	 * each cb call with the message just read on an IO_IN event
 	 */
-	void *msg;
+	void *rcv_buf;
+	/** points to a user buffer containing the next message to send */
+	const void *send_buf;
 };
 
 /**
  * Initializes a message source.
- * @param msg_src Message source to initialise
+ * @param msg_src Message source to initialize
  * @param fd File descriptor of the source
+ * @param type Type, in, out or duplex
+ * @param cb Callback called when a message has been received, stored in msg
+ * @param clean Cleanup callback, called when the source is removed from the
  * @param msg Buffer able to receive a message, filled at each cb call with the
  * message just read
  * @param len Size of the msg buffer
- * @param cb Callback called when a message has been received, stored in msg
- * @param clean Cleanup callback, called when the source is removed from the
  * monitor
- * @return errno compatible negative value
+ * @return errno compatible negative value on error, 0 on success
  */
-int io_src_msg_init(struct io_src_msg *msg_src, int fd, io_src_msg_cb_t *cb,
-		io_src_clean_t *clean, void *msg, unsigned len);
+int io_src_msg_init(struct io_src_msg *msg_src, int fd, enum io_src_event type,
+		io_src_msg_cb_t *cb, io_src_clean_t *clean, void *msg,
+		unsigned len);
--- libioutils/src/io_src_msg.c
+++ libioutils/src/io_src_msg.c
@@ -18,29 +18,75 @@
 #include "io_src_msg.h"
 
 /**
- * Source callback, reads the message and notifies the client
- * @param src Underlying monitor source of the message source
+ * Receives a message and notifies it to the client
+ * @param msg Message source
+ * @param fd Source's file descriptor
+ * @return Negative errno compatible value on error which implies source
+ * removal, positive errno compatible value for a warning, 0 on success
  */
-static int msg_cb(struct io_src *src)
+static int in_msg(struct io_src_msg *msg, int fd)
 {
 	ssize_t sret;
-	struct io_src_msg *msg = to_src_msg(src);
-
-	if (io_mon_has_error(src->events))
-		return -EIO;
 
 	/* get some data */
-	sret = TEMP_FAILURE_RETRY(read(src->fd, msg->msg, msg->len));
+	sret = TEMP_FAILURE_RETRY(read(fd, msg->rcv_buf, msg->len));
 	if (-1 == sret)
 		return -errno;
 	if ((ssize_t)msg->len != sret)
 		return -EIO;
 
-	return msg->cb(msg);
+	return msg->cb(msg, IO_IN);
 }
 
-int io_src_msg_init(struct io_src_msg *msg_src, int fd, io_src_msg_cb_t *cb,
-		io_src_clean_t *clean, void *msg, unsigned len)
+/**
+ * Asks the client, via it's callback, to provide a message, then writes it into
+ * the source's file descriptor.
+ * @param msg Message source
+ * @param fd Source's file descriptor
+ * @return Negative errno compatible value on error which implies source
+ * removal, positive errno compatible value for a warning, 0 on success
+ */
+static int out_msg(struct io_src_msg *msg, int fd)
+{
+	int ret;
+	ssize_t sret;
+
+	/* the client callback prepares us something to send */
+	ret = msg->cb(msg, IO_OUT);
+	if (0 > ret)
+		return ret;
+
+	/* msg->msg contains the message to send */
+	sret = TEMP_FAILURE_RETRY(write(fd, msg->send_buf, msg->len));
+	if (-1 == sret)
+		return -errno;
+
+	return 0;
+}
+/**
+ * Source callback, either performs in or out operation, depending on the event
+ * type
+ * @param src Underlying monitor source of the message source
+ * @return Negative errno compatible value on error which implies source
+ * removal, positive errno compatible value for a warning, 0 on success
+ */
+static int msg_cb(struct io_src *src)
+{
+	struct io_src_msg *msg = to_src_msg(src);
+
+	if (io_mon_has_error(src->events))
+		return -EIO;
+
+	if (src->events & IO_IN)
+		return in_msg(msg, src->fd);
+
+	return out_msg(msg, src->fd);
+}
+}
+
+int io_src_msg_init(struct io_src_msg *msg_src, int fd, enum io_src_event type,
+		io_src_msg_cb_t *cb, io_src_clean_t *clean, void *msg,
+		unsigned len)
 {
 	if (NULL == msg_src || -1 == fd || NULL == msg || NULL == cb ||
 			0 == len)
@@ -49,9 +95,9 @@ int io_src_msg_init(struct io_src_msg *msg_src, int fd, io_src_msg_cb_t *cb,
 	memset(msg_src, 0, sizeof(*msg_src));
 
 	msg_src->cb = cb;
-	msg_src->msg = msg;
+	msg_src->rcv_buf = msg;
 	msg_src->len = len;
 
 	/* can fail only on parameters */
-	return io_src_init(&(msg_src->src), fd, IO_IN, msg_cb, clean);
+	return io_src_init(&(msg_src->src), fd, type, msg_cb, clean);
 }
