32fb95000bfa1f8a79ce1668fc89785e3b85a1b4
Nicolas CARRIER <nicolas.carrier@parrot.com>
2013-02-15 18:14:30 +0100

    [DEV] Implementation of message source on Unix Abstract Seqpacket sockets

---
+++ libioutils/include/io_src_msg_uas.h
@@ -0,0 +1,87 @@
+/**
+ * @file io_src_msg_uas.h
+ * @date 25 oct. 2012
+ * @author nicolas.carrier@parrot.com
+ * @brief Source for reading / writing fixed length messages from a Unix socket
+ * with an Abstract name in Seqpacket mode (UAS)
+ *
+ * Copyright (C) 2012 Parrot S.A.
+ */
+#include <sys/un.h>
+
+#include "io_src_msg.h"
+
+/**
+ * @typedef io_src_msg_uas
+ * @brief Fixed length message source type, from an UAS
+ */
+struct io_src_msg_uas;
+
+#ifndef UNIX_PATH_MAX
+struct sockaddr_un sun_sizecheck;
+#define UNIX_PATH_MAX sizeof(sun_sizecheck.sun_path)
+#endif
+
+/**
+ * Callback called when I/O is possible.
+ * If evt is IO_OUT, must set next message to send, with
+ * io_src_msg_uas_set_next_message().
+ * If evt is IO_IN, the callback is called when a message has been read, stored
+ * in the rcv_buf specified at io_src_msg_uas_init().
+ * @param src Uas source
+ * @param evt Event type, either IO or OUT not both
+ * @return errno compatible value, positive for only a warning, negative if the
+ * source must be removed, 0 on success
+ */
+typedef int (io_src_msg_uas_cb_t)(struct io_src_msg_uas *src,
+		enum io_src_event evt);
+
+/**
+ * Called when the source is removed from a monitor, on error or at cleanup.
+ * Should perform client cleanup
+ * @param src Uas source
+ */
+typedef void (io_src_msg_uas_clean_t)(struct io_src_msg_uas *src);
+
+/**
+ * @typedef io_src_msg_uas
+ * @brief Message source type
+ */
+struct io_src_msg_uas {
+	/** inner msg monitor source */
+	struct io_src_msg src_msg;
+	/** user callback, notified when I/O is possible */
+	io_src_msg_uas_cb_t *cb;
+	/**
+	 * user clean callback, called when io_mon_clean is called , after the
+	 * underlying message source itself has been cleaned
+	 */
+	io_src_msg_uas_clean_t *clean;
+	/** path of the socket */
+	struct sockaddr_un addr;
+};
+
+/**
+ * Facility function to set the next message to be sent on the next "OUT" event.
+ * Must be used in user callback on such events.
+ * @param uas_src UAS source
+ * @param msg Points to a buffer containing the next message to send.
+ * @return errno compatible negative value on error, 0 on success
+ */
+int io_src_msg_uas_set_next_message(struct io_src_msg_uas *uas_src,
+		const void *rcv_buf);
+
+/**
+ * Initializes a bidirectional message source, from an UAS.
+ * @param uas_src Message source to initialize
+ * @param uas_src UAS source
+ * @param cb Callback notified
+ * @param clean Source cleanup callback
+ * @param rcv_buf Buffer where received data are stored
+ * @param len Size of rcv_buf
+ * @param fmt A la printf format string for the construction of the path
+ * @return errno compatible negative value
+ */
+int io_src_msg_uas_init(struct io_src_msg_uas *uas_src, io_src_msg_uas_cb_t *cb,
+		io_src_msg_uas_clean_t *clean, void *rcv_buf, unsigned len,
+		const char *fmt, ...) __attribute__((format(printf, 6, 7)));
---
+++ libioutils/src/io_src_msg_uas.c
@@ -0,0 +1,137 @@
+/**
+ * @file io_src_msg_uas.c
+ * @date 24 oct. 2012
+ * @author nicolas.carrier@parrot.com
+ * @brief Source for reading / writing fixed length messages
+ *
+ * Copyright (C) 2012 Parrot S.A.
+ */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif /* _GNU_SOURCE */
+#include <unistd.h>
+
+#include <errno.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#include <sys/socket.h>
+
+#include "io_mon.h"
+#include "io_src_msg_uas.h"
+
+/**
+ * @def to_src_msg_uas
+ * @brief Convert a source to it's message source container
+ */
+#define to_src_msg_uas(p) container_of(p, struct io_src_msg_uas, src_msg)
+
+/**
+ * Called when the underlying io_src_msg has I/O ready. Performs I/O via
+ * recvfrom / sendto
+ * @param src Source
+ * @param evt Either IO_IN or IO_OUT, not both
+ * @return errno compatible value, positive for only a warning, negative if the
+ * source must be removed, 0 on success
+ */
+static int uas_cb(struct io_src_msg *src, enum io_src_event evt)
+{
+	int ret;
+	ssize_t sret;
+	struct io_src_msg_uas *msg_uas_src = to_src_msg_uas(src);
+
+	if (IO_IN != evt && IO_OUT != evt)
+		return -EINVAL;
+
+	if (IO_IN == evt) {
+		sret = TEMP_FAILURE_RETRY(recvfrom(src->src.fd, src->rcv_buf,
+				src->len, 0, NULL, NULL));
+		if (-1 == sret)
+			return -errno;
+	}
+	ret = msg_uas_src->cb(msg_uas_src, evt);
+	if (0 > ret)
+		return ret;
+	if (IO_OUT == evt) {
+		sret = TEMP_FAILURE_RETRY(sendto(src->src.fd, src->send_buf,
+				src->len, 0,
+				(const struct sockaddr *)&(msg_uas_src->addr),
+				sizeof(msg_uas_src->addr)));
+		if (-1 == sret)
+			return -errno;
+	}
+
+	return ret;
+}
+
+/**
+ * Clean callback, called on io_src_clean, automatically on source removal due
+ * to error and monitor clean. Resets all the sources fields then calls user
+ * io_src_msg_uas_clean_t callback
+ * @param src Source to clean, previously cleaned by io_src_clean (i.e. with fd
+ * already closed)
+ */
+static void uas_clean(struct io_src_msg *src)
+{
+	struct io_src_msg_uas *uas = to_src_msg_uas(src);
+
+	uas->cb = NULL;
+	memset(&(uas->addr), 0, sizeof(uas->addr));
+
+	if (uas->clean)
+		uas->clean(uas);
+
+	uas->clean = NULL;
+}
+
+int io_src_msg_uas_set_next_message(struct io_src_msg_uas *uas_src,
+		const void *rcv_buf)
+{
+	if (NULL == uas_src)
+		return -EINVAL;
+
+	return io_src_msg_set_next_message(&(uas_src->src_msg), rcv_buf);
+}
+
+int io_src_msg_uas_init(struct io_src_msg_uas *uas_src, io_src_msg_uas_cb_t *cb,
+		io_src_msg_uas_clean_t *clean, void *rcv_buf, unsigned len,
+		const char *fmt, ...)
+{
+	int sockfd;
+	va_list args;
+	int ret;
+
+	if (NULL == uas_src || NULL == rcv_buf || 0 == len || NULL == cb ||
+			NULL == fmt || '\0' == *fmt)
+		return -EINVAL;
+
+	memset(uas_src, 0, sizeof(*uas_src));
+
+	sockfd = socket(AF_UNIX, SOCK_DGRAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0);
+	if (sockfd < 0)
+		return -errno;
+
+	uas_src->addr.sun_path[0] = '\0';
+	va_start(args, fmt);
+	vsnprintf(uas_src->addr.sun_path + 1, UNIX_PATH_MAX - 1, fmt, args);
+	va_end(args);
+	uas_src->addr.sun_family = AF_UNIX;
+	ret = bind(sockfd, (struct sockaddr *) &(uas_src->addr),
+			sizeof(uas_src->addr));
+	if (ret < 0) {
+		ret = -errno;
+		goto out;
+	}
+
+	uas_src->cb = cb;
+	uas_src->clean = clean;
+
+	/* can fail only on parameters */
+	return io_src_msg_init(&(uas_src->src_msg), sockfd, IO_DUPLEX, uas_cb,
+			uas_clean, rcv_buf, len, 0);
+
+out:
+	close(sockfd);
+	return -1;
+}
