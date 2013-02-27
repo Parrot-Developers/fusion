d7ec895c0426234f1ba911b2c3b67231018dd9ed
Nicolas CARRIER <nicolas.carrier@parrot.com>
2013-02-27 15:31:18 +0100

    [BUILD] added LOCAL_CATEGORY_PATH and LOCAL_DESCRIPTION

--- libioutils/atom.mk
+++ libioutils/atom.mk
@@ -7,6 +7,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libioutils
+LOCAL_DESCRIPTION := Utils for event loop based asynchronous I/O
+LOCAL_CATEGORY_PATH := libs
 
 LOCAL_EXPORT_C_INCLUDES  := $(LOCAL_PATH)/include
 
