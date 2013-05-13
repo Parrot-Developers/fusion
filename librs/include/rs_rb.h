fccc1e2a7f2ce4e22fa2e1103780aaa243d8d915
Nicolas CARRIER <nicolas.carrier@parrot.com>
2013-05-13 10:42:16 +0200

    [DEV][librs][rb] replaced static inline implementation with extern ones

--- librs/include/rs_rb.h
+++ librs/include/rs_rb.h
@@ -11,9 +11,8 @@
 
 #ifndef RS_RB_H_
 #define RS_RB_H_
+
 #include <stdint.h>
-#include <assert.h>
-#include <stdlib.h>
 
 /* ring buffer */
 struct rs_rb {
@@ -25,118 +24,48 @@ struct rs_rb {
 	size_t write;		/* write offset */
 };
 
-static int isPowerOfTwo(unsigned int x)
-{
-	return ((x != 0) && ((x & (~x + 1)) == x));
-}
-
-static inline int rs_rb_init(struct rs_rb *rb, void *buffer, size_t size)
-{
-	/* size must be power of two */
-	if (!isPowerOfTwo(size) || NULL == rb || NULL == buffer)
-		return -EINVAL;
-
-	rb->base = buffer;
-	rb->size = size;
-	rb->size_mask = size - 1;
-	rb->read = 0;
-	rb->write = 0;
-	rb->len = 0;
-
-	return 0;
-}
+int rs_rb_init(struct rs_rb *rb, void *buffer, size_t size);
 
 /* get ring buffer size */
-static inline size_t rs_rb_size(struct rs_rb *rb)
-{
-	return rb->size;
-}
+size_t rs_rb_size(struct rs_rb *rb);
 
 /* empty ring buffer */
-static inline void rs_rb_empty(struct rs_rb *rb)
-{
-	rb->read = 0;
-	rb->write = 0;
-	rb->len = 0;
-}
+void rs_rb_empty(struct rs_rb *rb);
 
 /* resets a ring buffer */
-static inline void rs_rb_clean(struct rs_rb *rb)
-{
-	memset(rb, 0, sizeof(*rb));
-}
+void rs_rb_clean(struct rs_rb *rb);
 
-/**
- *  ring buffer read functions
- **/
+/* *  ring buffer read functions * */
 
 /* get ring buffer read ptr */
-static inline void *rs_rb_read_ptr(struct rs_rb *rb)
-{
-	return (uint8_t *) rb->base + rb->read;
-}
+void *rs_rb_read_ptr(struct rs_rb *rb);
 
 /* get ring buffer read length (number of bytes that can be read) */
-static inline size_t rs_rb_read_length(struct rs_rb *rb)
-{
-	return rb->len;
-}
+size_t rs_rb_read_length(struct rs_rb *rb);
 
 /* get ring buffer read length with out wrapping
  * (number of bytes that can be read from read ptr without wrapping) */
-static inline size_t rs_rb_read_length_no_wrap(struct rs_rb *rb)
-{
-	return (rb->write >= rb->read) ? rb->len : rb->len - rb->write;
-}
+size_t rs_rb_read_length_no_wrap(struct rs_rb *rb);
 
 /* increment read pointer, freeing up ring buffer space */
-static inline void rs_rb_read_incr(struct rs_rb *rb, size_t length)
-{
-	assert(length <= rb->len);
-	rb->len -= length;
-	rb->read += length;
-	rb->read &= rb->size_mask;
-}
+void rs_rb_read_incr(struct rs_rb *rb, size_t length);
 
 /* read byte at offset  */
-static inline uint8_t rs_rb_read_offset(struct rs_rb *rb, size_t offset)
-{
-	assert(offset < rb->len);
-	return ((uint8_t *) rb->base)[(rb->read + offset) & rb->size_mask];
-}
-/**
- *  ring buffer write functions
- **/
+uint8_t rs_rb_read_offset(struct rs_rb *rb, size_t offset);
+
+/* * ring buffer write functions * */
 
 /* get ring buffer write ptr */
-static inline void *rs_rb_write_ptr(struct rs_rb *rb)
-{
-	return (uint8_t *) rb->base + rb->write;
-}
+void *rs_rb_write_ptr(struct rs_rb *rb);
 
 /* get ring buffer write length (number of bytes that can be written) */
-static inline size_t rs_rb_write_length(struct rs_rb *rb)
-{
-	return rb->size - rb->len;
-}
+size_t rs_rb_write_length(struct rs_rb *rb);
 
 /* get ring buffer write length with out wrapping
  * (number of bytes that can be written from write ptr without wrapping) */
-static inline size_t rs_rb_write_length_no_wrap(struct rs_rb *rb)
-{
-	if (rb->write >= rb->read)
-		return rb->size - rb->write;
-	else
-		return rb->read - rb->write;
-}
+size_t rs_rb_write_length_no_wrap(struct rs_rb *rb);
 
 /* increment write pointer */
-static inline void rs_rb_write_incr(struct rs_rb *rb, size_t length)
-{
-	assert(rb->len + length <= rb->size);
-	rb->len += length;
-	rb->write += length;
-	rb->write &= rb->size_mask;
-}
+void rs_rb_write_incr(struct rs_rb *rb, size_t length);
 
 #endif /* RS_RB_H_ */
---
+++ librs/src/rs_rb.c
@@ -0,0 +1,112 @@
+/**
+ * @file rs_rb.c
+ * @date 13 mai 2013
+ * @author Jean-Baptiste Dubois
+ * @author nicolas.carrier@parrot.com
+ * @brief ring buffer implementation, imported from mambo
+ *
+ * Copyright (C) 2013 Parrot S.A.
+ */
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+
+#include <rs_rb.h>
+
+/**
+ * Says whether a number is a power of two or not
+ * @param x Number to test
+ * @return non-zero if the number is a power of two
+ */
+static int is_power_of_two(unsigned int x)
+{
+	return ((x != 0) && ((x & (~x + 1)) == x));
+}
+
+int rs_rb_init(struct rs_rb *rb, void *buffer, size_t size)
+{
+	/* size must be power of two */
+	if (!is_power_of_two(size) || NULL == rb || NULL == buffer)
+		return -EINVAL;
+
+	rb->base = buffer;
+	rb->size = size;
+	rb->size_mask = size - 1;
+	rb->read = 0;
+	rb->write = 0;
+	rb->len = 0;
+
+	return 0;
+}
+
+size_t rs_rb_size(struct rs_rb *rb)
+{
+	return rb->size;
+}
+
+void rs_rb_empty(struct rs_rb *rb)
+{
+	rb->read = 0;
+	rb->write = 0;
+	rb->len = 0;
+}
+
+void rs_rb_clean(struct rs_rb *rb)
+{
+	memset(rb, 0, sizeof(*rb));
+}
+
+void *rs_rb_read_ptr(struct rs_rb *rb)
+{
+	return (uint8_t *) rb->base + rb->read;
+}
+
+size_t rs_rb_read_length(struct rs_rb *rb)
+{
+	return rb->len;
+}
+
+size_t rs_rb_read_length_no_wrap(struct rs_rb *rb)
+{
+	return (rb->write >= rb->read) ? rb->len : rb->len - rb->write;
+}
+
+void rs_rb_read_incr(struct rs_rb *rb, size_t length)
+{
+	assert(length <= rb->len);
+	rb->len -= length;
+	rb->read += length;
+	rb->read &= rb->size_mask;
+}
+
+uint8_t rs_rb_read_offset(struct rs_rb *rb, size_t offset)
+{
+	assert(offset < rb->len);
+	return ((uint8_t *) rb->base)[(rb->read + offset) & rb->size_mask];
+}
+
+void *rs_rb_write_ptr(struct rs_rb *rb)
+{
+	return (uint8_t *) rb->base + rb->write;
+}
+
+size_t rs_rb_write_length(struct rs_rb *rb)
+{
+	return rb->size - rb->len;
+}
+
+size_t rs_rb_write_length_no_wrap(struct rs_rb *rb)
+{
+	if (rb->write >= rb->read)
+		return rb->size - rb->write;
+	else
+		return rb->read - rb->write;
+}
+
+void rs_rb_write_incr(struct rs_rb *rb, size_t length)
+{
+	assert(rb->len + length <= rb->size);
+	rb->len += length;
+	rb->write += length;
+	rb->write &= rb->size_mask;
+}
