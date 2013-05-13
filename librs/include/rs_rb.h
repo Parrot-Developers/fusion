a46f9b485796c034863652e7e991fa35d1d93aad
Nicolas CARRIER <nicolas.carrier@parrot.com>
2013-05-13 15:09:06 +0200

    [DOC][librs][rb] complete doxygen for the API

--- librs/include/rs_rb.h
+++ librs/include/rs_rb.h
@@ -3,9 +3,8 @@
  *
  * @brief ring buffer implementation, imported from mambo
  *
- * read means data read, written to the ring buffer
- *
- * write means data which can be read from the ring buffer, written to an output
+ * all operations are from the point of view of the ring buffer (i.e. write is
+ * write to the ring buffer)
  *
  * Copyright (C) 2011 Parrot S.A.
  *
@@ -18,58 +17,127 @@
 
 #include <stdint.h>
 
-/* ring buffer */
+/**
+ * @struct rs_rb
+ * @brief Ring buffer structure
+ */
 struct rs_rb {
-	void *base;		/* base address of memory buffer */
-	size_t size;		/* size of memory buffer */
-	size_t size_mask;	/* size mask of memory buffer */
-	size_t len;		/* buffer length (bytes ready to be read) */
-	size_t read;		/* read offset */
-	size_t write;		/* write offset */
+	void *base;		/** base address of memory buffer */
+	size_t size;		/** size of memory buffer */
+	size_t size_mask;	/** size mask of memory buffer */
+	size_t len;		/** buffer length i.e. bytes stored in the rb */
+	size_t read;		/** read offset */
+	size_t write;		/** write offset */
 };
 
+/**
+ * Initializes a ring buffer, with a buffer and it's size
+ * @param rb Ring buffer to initialize
+ * @param buffer Buffer used by the ring buffer
+ * @param size Size of the buffer, must be a power of two
+ * @return -1 on error, 0 otherwise
+ */
 int rs_rb_init(struct rs_rb *rb, void *buffer, size_t size);
 
-/* get ring buffer size */
+/**
+ * Get the ring buffer size. i.e. the total number of bytes the buffer can
+ * contain
+ * @param rb Ring buffer
+ * @return The ring buffer size
+ */
 size_t rs_rb_get_size(struct rs_rb *rb);
 
-/* empty ring buffer */
+/**
+ * Empty a buffer
+ * @param rb Ring buffer
+ * @return non-zero on error (NULL pointer), 0 otherwise
+ */
 int rs_rb_empty(struct rs_rb *rb);
 
-/* resets a ring buffer */
+/**
+ * Resets a ring buffer for future reuse
+ * @param rb
+ */
 int rs_rb_clean(struct rs_rb *rb);
 
 /* *  ring buffer read functions * */
 
-/* get ring buffer read ptr */
+/**
+ * Get ring buffer read ptr, i.e. where data can be consumed from the ring
+ * buffer
+ * @param rb Ring buffer
+ * @return place in the buffer, when data can be read from
+ * @note not more than the return of rs_rb_get_read_length_no_wrap() must be
+ * read from here.
+ */
 void *rs_rb_get_read_ptr(struct rs_rb *rb);
 
-/* get ring buffer read length, i.e. free space left in the ring buffer */
+/**
+ * Get the ring buffer read length, i.e. bytes stored in the ring buffer,
+ * available to read from it
+ * @param rb Ring buffer
+ * @return Data available to read from the buffer, 0 if non, or error
+ */
 size_t rs_rb_get_read_length(struct rs_rb *rb);
 
-/* get ring buffer read length with out wrapping
- * (number of bytes that can be read from read ptr without wrapping) */
+/**
+ * Get ring buffer read length without wrapping to the start of the buffer
+ * @param rb Ring buffer
+ * @return Size which can be read from the buffer, without wrapping i.e. in a
+ * consecutive manner
+ */
 size_t rs_rb_get_read_length_no_wrap(struct rs_rb *rb);
 
-/* increment read pointer, freeing up ring buffer space */
+/**
+ * Increment read pointer, freeing up ring buffer space
+ * @param rb Ring buffer
+ * @param length Amount of data we have consumed from the ring buffer
+ * @return non-zero negative errno-compatible value on error, 0 otherwise
+ */
 int rs_rb_read_incr(struct rs_rb *rb, size_t length);
 
-/* read byte at offset  */
+/**
+ * Read a byte at a given offset
+ * @param rb Ring buffer
+ * @param offset Offset of the byte to read
+ * @param value Must point to a storage for the value to retrieve. Can't be NULL
+ * @return non-zero negative errno-compatible value on error, 0 otherwise
+ */
 int rs_rb_read_at(struct rs_rb *rb, size_t offset, uint8_t *value);
 
 /* * ring buffer write functions * */
 
-/* get ring buffer write ptr */
+/**
+ * get ring buffer write ptr, i.e. the place where data can be read to the rb.
+ * @param rb Ring buffer
+ * @return NULL on error, place to read to otherwise
+ * @note not more than the return of rs_rb_get_write_length_no_wrap() must be
+ * written
+ */
 void *rs_rb_get_write_ptr(struct rs_rb *rb);
 
-/* get ring buffer write length (number of bytes that can be written) */
+/**
+ * get ring buffer write room left
+ * @param rb Ring buffer
+ * @return Number of bytes which can't still be stored into the ring buffer. 0
+ * on error
+ */
 size_t rs_rb_get_write_length(struct rs_rb *rb);
 
-/* get ring buffer write length with out wrapping
- * (number of bytes that can be written from write ptr without wrapping) */
+/**
+ * get ring buffer write length with out wrapping i.e., the number of bytes that
+ * can be written to the buffer without wrapping
+ * @param rb Ring buffer
+ * @return Amount of data which can be written linearly in one chunk. 0 on error
+ */
 size_t rs_rb_get_write_length_no_wrap(struct rs_rb *rb);
 
-/* increment write pointer */
+/**
+ * increment write pointer, i.e., data have been written th the write buffer
+ * @param rb Ring buffer
+ * @param length Amount of data written
+ * @return non-zero negative errno-compatible value on error, 0 otherwise
+ */
 int rs_rb_write_incr(struct rs_rb *rb, size_t length);
 
 #endif /* RS_RB_H_ */
