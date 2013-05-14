49c6b52d8dbf36f16fba44f08ee0bc0d49b2206c
Nicolas CARRIER <nicolas.carrier@parrot.com>
2013-05-14 11:50:56 +0200

    [DEV][librs][hmap] unit tests implementation

--- librs/tests/rs_hmap_test.c
+++ librs/tests/rs_hmap_test.c
@@ -6,25 +6,181 @@
  *
  * Copyright (C) 2013 Parrot S.A.
  */
+#include <errno.h>
+
 #include <CUnit/Basic.h>
 
 #include <fautes.h>
 
+#include <rs_hmap.h>
+
 static void testRS_HMAP_INIT(void)
 {
-	/* initialization */
+	int ret;
+	struct rs_hmap map;
 
 	/* normal use cases */
+	ret = rs_hmap_init(&map, 10);
+	CU_ASSERT_EQUAL(ret, 0);
+	CU_ASSERT(map.size >= 10);
+	CU_ASSERT_PTR_NOT_NULL(map.buckets);
+	rs_hmap_clean(&map);
 
 	/* error use cases */
+	ret = rs_hmap_init(&map, RS_HMAP_PRIME_MAX + 1U);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_init(NULL, 10);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+}
+
+static void testRS_HMAP_CLEAN(void)
+{
+	int ret;
+	struct rs_hmap map;
+
+	/* normal use cases */
+	ret = rs_hmap_init(&map, 10);
+	CU_ASSERT_EQUAL(ret, 0);
+	ret = rs_hmap_clean(&map);
+	CU_ASSERT_EQUAL(ret, 0);
+
+	/* error use cases */
+	ret = rs_hmap_clean(NULL);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+}
+
+static void testRS_HMAP_LOOKUP(void)
+{
+	int ret;
+	struct rs_hmap map;
+	void *data1 = (void *)42;
+	void *data2 = (void *)66;
+	void *needle = NULL;
+
+	/* initialization */
+	ret = rs_hmap_init(&map, 10);
+	CU_ASSERT_EQUAL(ret, 0);
+	ret = rs_hmap_insert(&map, "ursule", data1);
+	CU_ASSERT_EQUAL(ret, 0);
+	ret = rs_hmap_insert(&map, "gédéon", data2);
+	CU_ASSERT_EQUAL(ret, 0);
+
+	/* normal use cases */
+	ret = rs_hmap_lookup(&map, "ursule", &needle);
+	CU_ASSERT_EQUAL(ret, 0);
+	CU_ASSERT_PTR_EQUAL(needle, data1);
+	ret = rs_hmap_lookup(&map, "gédéon", &needle);
+	CU_ASSERT_EQUAL(ret, 0);
+	CU_ASSERT_PTR_EQUAL(needle, data2);
+	ret = rs_hmap_lookup(&map, "frénégonde", &needle);
+	CU_ASSERT_EQUAL(ret, -ENOENT);
+	CU_ASSERT_PTR_EQUAL(needle, NULL);
+
+	/* error use cases */
+	ret = rs_hmap_lookup(NULL, "ursule", &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_lookup(&map, NULL, &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_lookup(&map, "", &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_lookup(&map, "ursule", NULL);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
 
 	/* cleanup */
+	rs_hmap_clean(&map);
+}
+
+static void testRS_HMAP_INSERT(void)
+{
+	int ret;
+	struct rs_hmap map;
+	void *needle = NULL;
+
+	/* initialization */
+	ret = rs_hmap_init(&map, 10);
+	CU_ASSERT_EQUAL(ret, 0);
+
+	/* normal use cases */
+	/* other use cases are already tested in lookup */
+	/* inserting NULL is allowed */
+	ret = rs_hmap_insert(&map, "ursule", NULL);
+	CU_ASSERT_EQUAL(ret, 0);
+
+	/* error use cases */
+	ret = rs_hmap_insert(NULL, "ursule", &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_insert(&map, NULL, &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_insert(&map, "", &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	rs_hmap_clean(&map);
+	/* searching in a hash map with no bucket must fail cleanly */
+	ret = rs_hmap_insert(&map, "ursule", &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+}
+
+static void testRS_HMAP_REMOVE(void)
+{
+	int ret;
+	struct rs_hmap map;
+	void *data1 = (void *)42;
+	void *data2 = (void *)66;
+	void *needle = NULL;
+
+	/* initialization */
+	ret = rs_hmap_init(&map, 10);
+	CU_ASSERT_EQUAL(ret, 0);
+	ret = rs_hmap_insert(&map, "ursule", data1);
+	CU_ASSERT_EQUAL(ret, 0);
+	ret = rs_hmap_insert(&map, "gédéon", data2);
+	CU_ASSERT_EQUAL(ret, 0);
+
+	/* normal use cases */
+	ret = rs_hmap_remove(&map, "ursule", &needle);
+	CU_ASSERT_EQUAL(ret, 0);
+	CU_ASSERT_PTR_EQUAL(needle, data1);
+	/* if we wan't to drop data, needle can be NULL */
+	ret = rs_hmap_remove(&map, "gédéon", NULL);
+	CU_ASSERT_EQUAL(ret, 0);
+	ret = rs_hmap_remove(&map, "ursule", &needle);
+	CU_ASSERT_EQUAL(ret, -ENOENT);
+	CU_ASSERT_PTR_EQUAL(needle, NULL);
+	ret = rs_hmap_remove(&map, "frénégonde", &needle);
+	CU_ASSERT_EQUAL(ret, -ENOENT);
+	CU_ASSERT_PTR_EQUAL(needle, NULL);
+
+	/* error use cases */
+	ret = rs_hmap_remove(NULL, "ursule", &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_remove(&map, NULL, &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+	ret = rs_hmap_remove(&map, "", &needle);
+	CU_ASSERT_NOT_EQUAL(ret, 0);
+
+	/* cleanup */
+	rs_hmap_clean(&map);
 }
 
 static const struct test_t tests[] = {
 		{
 				.fn = testRS_HMAP_INIT,
 				.name = "rs_hmap_init"
+		},
+		{
+				.fn = testRS_HMAP_CLEAN,
+				.name = "rs_hmap_clean"
+		},
+		{
+				.fn = testRS_HMAP_LOOKUP,
+				.name = "rs_hmap_lookup"
+		},
+		{
+				.fn = testRS_HMAP_INSERT,
+				.name = "rs_hmap_insert"
+		},
+		{
+				.fn = testRS_HMAP_REMOVE,
+				.name = "rs_hmap_remove"
 		},
 
 		/* NULL guard */
