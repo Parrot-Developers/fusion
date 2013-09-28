d65da90b94f002c2c7f9cd345a82ad2618486ddc
Nicolas CARRIER <nicolas@robert>
2013-09-28 08:52:34 +0200

    [DEV][io] added simple use case unit test for io module
    Change-Id: I960d43a9fa2186d535ecbe646c33dae15ee30f8c

--- libioutils/tests/io_io_test.c
+++ libioutils/tests/io_io_test.c
@@ -71,7 +71,7 @@ static void testIO_DESTROY(void)
 	ret = socketpair(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0,
 			sockets);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
-	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 0);
+	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 1);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
 
 	/* normal use cases */
@@ -169,7 +169,7 @@ static void testIO_READ_STOP(void)
 	ret = socketpair(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0,
 			sockets);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
-	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 0);
+	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 1);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
 	ret = io_io_read_start(&io, io_cb, (void *)42, 0);
 	CU_ASSERT_EQUAL(ret, 0);
@@ -209,12 +209,12 @@ static void testIO_READ_STATE(void)
 	ret = socketpair(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0,
 			sockets);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
-	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 0);
+	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 1);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
-
-	/* normal use cases */
 	ret = io_io_read_start(&io, io_cb, (void *)42, 0);
 	CU_ASSERT_EQUAL(ret, 0);
+
+	/* normal use cases */
 	state = io_io_read_state(&io);
 	CU_ASSERT_EQUAL(state, IO_IO_STARTED);
 	ret = io_io_read_stop(&io);
@@ -235,45 +235,14 @@ static void testIO_READ_STATE(void)
 
 static void testIO_WRITE_ADD(void)
 {
-	int ret;
-	int sockets[2];
-	struct io_mon mon;
-	struct io_io io;
-	enum io_io_state state;
-	int io_cb(struct io_io *io, struct rs_rb *rb, size_t newbytes,
-			void *data)
-	{
-
-		return 0;
-	}
 	/* initialization */
-	ret = io_mon_init(&mon);
-	CU_ASSERT_EQUAL_FATAL(ret, 0);
-	ret = socketpair(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0,
-			sockets);
-	CU_ASSERT_EQUAL_FATAL(ret, 0);
-	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 0);
-	CU_ASSERT_EQUAL_FATAL(ret, 0);
+	fprintf(stderr, "%s STUBBED !!!\n", __func__);
 
 	/* normal use cases */
-	ret = io_io_read_start(&io, io_cb, (void *)42, 0);
-	CU_ASSERT_EQUAL(ret, 0);
-	state = io_io_read_state(&io);
-	CU_ASSERT_EQUAL(state, IO_IO_STARTED);
-	ret = io_io_read_stop(&io);
-	CU_ASSERT_EQUAL(ret, 0);
-	state = io_io_read_state(&io);
-	CU_ASSERT_EQUAL(state, IO_IO_STOPPED);
 
 	/* error use cases */
-	state = io_io_read_state(NULL);
-	CU_ASSERT_EQUAL(state, IO_IO_ERROR);
 
 	/* cleanup */
-	io_io_destroy(&io);
-	io_mon_clean(&mon);
-	io_close(sockets + 0);
-	io_close(sockets + 1);
 }
 
 static void testIO_ABORT(void)
@@ -286,6 +255,163 @@ static void testIO_ABORT(void)
 	/* error use cases */
 
 	/* cleanup */
+}
+
+static void testIO_SIMPLE_USE_CASE(void)
+{
+#define MSG1 "first message"
+#define ANS1 "first answer"
+#define MSG2 "second message"
+#define ANS2 "second answer"
+#define BUF_SIZE 1024
+#define STATE_START 0
+#define STATE_MSG1_RECEIVED 1
+#define STATE_MSG2_RECEIVED 2
+#define STATE_ANS1_RECEIVED 4
+#define STATE_ANS2_RECEIVED 8
+#define STATE_ALL_DONE 15
+	int state = STATE_START;
+	int ret;
+	struct timeval timeout;
+	int sockets[2];
+	struct io_mon mon;
+	struct io_io io;
+	struct io_src sock_src;
+	int monfd;
+	fd_set rfds;
+	int count = 0;
+	void io_buf_write_cb(struct io_io_write_buffer *buffer,
+		enum io_io_write_status status)
+	{
+		count++;
+		// stubbed !!! TODO
+	}
+	struct io_io_write_buffer io_buffers[2] = {
+			[0] = {
+				.node = {
+						.next = NULL,
+						.prev = NULL,
+				},
+				.cb = io_buf_write_cb,
+				.data = (void *)666,
+				.length = sizeof(MSG1),
+				.address = MSG1,
+			},
+			[1] = {
+				.node = {
+						.next = NULL,
+						.prev = NULL,
+				},
+				.cb = io_buf_write_cb,
+				.data = (void *)421,
+				.length = sizeof(MSG2),
+				.address = MSG2,
+			},
+	};
+	int io_cb(struct io_io *io, struct rs_rb *rb, size_t newbytes,
+			void *data)
+	{
+		if (strncmp(rs_rb_get_read_ptr(rb), ANS1, sizeof(ANS1)) == 0) {
+			CU_ASSERT_EQUAL(newbytes, sizeof(ANS1));
+			CU_ASSERT(!(STATE_ANS1_RECEIVED & state));
+			state |= STATE_ANS1_RECEIVED;
+			rs_rb_read_incr(rb, newbytes);
+		} else if (strncmp(rs_rb_get_read_ptr(rb), ANS2,
+				sizeof(ANS2)) == 0) {
+			CU_ASSERT_EQUAL(newbytes, sizeof(ANS2));
+			CU_ASSERT(!(STATE_ANS2_RECEIVED & state));
+			state |= STATE_ANS2_RECEIVED;
+			rs_rb_read_incr(rb, newbytes);
+		}
+
+		return 0;
+	}
+	void sock_cb(struct io_src *src)
+	{
+		char buf[1024];
+		ssize_t sret;
+
+		sret = read(io_src_get_fd(src), buf, 1024);
+		CU_ASSERT(sret > 0);
+
+		if (STATE_START == state) {
+			CU_ASSERT_STRING_EQUAL(buf, MSG1);
+			sret = write(io_src_get_fd(src), ANS1, sizeof(ANS1));
+			CU_ASSERT(sret > 0);
+			CU_ASSERT(!(STATE_MSG1_RECEIVED & state));
+			state |= STATE_MSG1_RECEIVED;
+			CU_ASSERT_EQUAL(count, 1);
+			ret = io_io_write_add(&io, io_buffers + 1);
+			CU_ASSERT_EQUAL(ret, 0);
+		} else {
+			CU_ASSERT_STRING_EQUAL(buf, MSG2);
+			sret = write(io_src_get_fd(src), ANS2, sizeof(ANS2));
+			CU_ASSERT(sret > 0);
+			CU_ASSERT(!(STATE_MSG2_RECEIVED & state));
+			state |= STATE_MSG2_RECEIVED;
+		}
+	}
+
+	/* initialization */
+	ret = io_mon_init(&mon);
+	CU_ASSERT_EQUAL_FATAL(ret, 0);
+	ret = socketpair(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0,
+			sockets);
+	CU_ASSERT_EQUAL_FATAL(ret, 0);
+	ret = io_src_init(&sock_src, sockets[1], IO_IN, sock_cb);
+	CU_ASSERT_EQUAL_FATAL(ret, 0);
+	ret = io_mon_add_source(&mon, &sock_src);
+	CU_ASSERT_EQUAL_FATAL(ret, 0);
+	ret = io_io_create(&io, &mon, SUITE_NAME, sockets[0], sockets[0], 1);
+	CU_ASSERT_EQUAL_FATAL(ret, 0);
+	ret = io_io_read_start(&io, io_cb, (void *)42, 0);
+	CU_ASSERT_EQUAL(ret, 0);
+
+	/* simple test case, send two messages and read two */
+	ret = io_io_write_add(&io, io_buffers + 0);
+	CU_ASSERT_EQUAL(ret, 0);
+
+	monfd = io_mon_get_fd(&mon);
+	while (STATE_ALL_DONE != state) {
+		/* restore the timer */
+		timeout.tv_sec = 3;
+		timeout.tv_usec = 0;
+
+		FD_ZERO(&rfds);
+		FD_SET(monfd, &rfds);
+
+		ret = select(monfd + 1, &rfds, NULL, NULL, &timeout);
+		/* error, not normal */
+		CU_ASSERT_NOT_EQUAL(ret, -1);
+		if (-1 == ret)
+			goto out;
+
+		/* timeout, not normal */
+		CU_ASSERT_NOT_EQUAL(ret, 0);
+		if (0 == ret)
+			goto out;
+		ret = io_mon_process_events(&mon);
+		CU_ASSERT_EQUAL(ret, 0);
+		if (0 != ret)
+			goto out;
+	}
+
+out:
+	CU_ASSERT(STATE_MSG1_RECEIVED & state);
+	CU_ASSERT(STATE_MSG2_RECEIVED & state);
+	CU_ASSERT(STATE_ANS1_RECEIVED & state);
+	CU_ASSERT(STATE_ANS2_RECEIVED & state);
+	CU_ASSERT(ret > -1);
+	CU_ASSERT_EQUAL(count, 2);
+
+	/* cleanup */
+	io_io_read_stop(&io);
+	io_mon_remove_source(&mon, &sock_src);
+	io_src_clean(&sock_src);
+	io_io_destroy(&io);
+	io_mon_clean(&mon);
+	io_close(sockets + 0);
+	io_close(sockets + 1);
 }
 
 static const struct test_t tests[] = {
@@ -324,6 +450,10 @@ static const struct test_t tests[] = {
 		{
 				.fn = testIO_ABORT,
 				.name = "io_io_write_abort"
+		},
+		{
+				.fn = testIO_SIMPLE_USE_CASE,
+				.name = "io_simple_use_case"
 		},
 
 		/* NULL guard */
