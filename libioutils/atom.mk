5ce7c72387706cf564b274caad873a1b0f3ef5d8
Nicolas CARRIER <nicolas.carrier@parrot.com>
2013-10-23 11:23:31 +0200

    [BUILD][io] added integration into test framework
    Change-Id: I13d616e9516a38678d542782ad2492c827f17aa6

--- libioutils/atom.mk
+++ libioutils/atom.mk
@@ -16,7 +16,7 @@ LOCAL_SRC_FILES := $(call all-c-files-under,src)
 
 LOCAL_LIBRARIES := librs libpidwatch
 
-ifdef LIBIOUTILS_FAUTES_SUPPORT
+ifdef TARGET_TEST
 LOCAL_SRC_FILES += $(call all-c-files-under,tests)
 
 LOCAL_LIBRARIES += libfautes
@@ -24,3 +24,24 @@ endif
 
 include $(BUILD_SHARED_LIBRARY)
 
+###############################################################################
+# tst-libioutils
+###############################################################################
+
+ifdef TARGET_TEST
+include $(CLEAR_VARS)
+
+LOCAL_CATEGORY_PATH := tests
+
+TESTED_MODULE := libioutils
+
+LOCAL_MODULE := tst-$(TESTED_MODULE)
+
+LOCAL_REQUIRED_MODULES := fautes $(TESTED_MODULE)
+
+LOCAL_COPY_FILES := \
+	tests/tst-01.sh:tests/bin/$(TESTED_MODULE)/ \
+	tests/email_notification:tests/bin/$(TESTED_MODULE)/
+
+include $(BUILD_CUSTOM)
+endif # TARGET_TEST
