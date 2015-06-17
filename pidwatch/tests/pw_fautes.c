4f3cc5ce9675cb9db867ebaf0b082b8655541e5b
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2015-06-17 22:11:01 +0200

    [DEV][pw] support running tests by executing the shared object
    Change-Id: I161e0b819e91e0feda950f0795ac9fc4bba835e2

--- pidwatch/tests/pw_fautes.c
+++ pidwatch/tests/pw_fautes.c
@@ -1,14 +1,19 @@
 /**
- * @file io_fautes.c
+ * @file pw_fautes.c
  * @date 29 apr. 2013
  * @author nicolas.carrier@parrot.com
  * @brief Automated unit tests libpidwatch. Definitions for Fautes support
  *
  * Copyright (C) 2013 Parrot S.A.
  */
+#include <unistd.h>
+
 #include <fautes.h>
 
 #include "pw_fautes.h"
+
+/* set an interpreter section so that this lib can be run as an executable */
+const char pw_interp[] __attribute__((section(".interp"))) = FUSION_INTERPRETER;
 
 struct suite_t *libpidwatch_test_suites[] = {
 		&pidwatch_suite,
@@ -26,3 +31,12 @@ struct pool_t fautes_pool = {
 	.initializer = libpidwatch_pool_initializer,
 	.suites = libpidwatch_test_suites,
 };
+
+void libpidwatch_tests(void)
+{
+	int ret;
+
+	ret = fautes_run_test_pool(&fautes_pool, false);
+
+	_exit(ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+}
