ebe5e80cccb32286d74a6945ac39a549333378d3
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-20 15:33:17 +0100

    [DEV][io] removed nested functions from pid src tests
    Change-Id: Ib120a1baf4ef3ca7a99ecf3426f635c6cfcc08b1

--- libioutils/tests/io_src_pid_test.c
+++ libioutils/tests/io_src_pid_test.c
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <errno.h>
 
 #include <CUnit/Basic.h>
 
@@ -80,36 +81,45 @@ static pid_t __attribute__((sentinel)) launch(const char *prog, ...)
 	return pid;
 }
 
+struct my_pid_src {
+	struct io_src_pid pid_src;
+	bool process_dead;
+};
+
+static void cb(struct io_src_pid *src_pid, pid_t local_pid, int status)
+{
+	int wait_status;
+	struct my_pid_src *s = ut_container_of(src_pid, struct my_pid_src,
+			pid_src);
+
+	CU_ASSERT(WIFEXITED(status));
+	CU_ASSERT_EQUAL(WEXITSTATUS(status), 0);
+	s->process_dead = true;
+	waitpid(local_pid, &wait_status, 0);
+	CU_ASSERT_EQUAL(status, wait_status);
+};
+
 static void testSRC_PID_INIT(void)
 {
 	int ret;
 	fd_set rfds;
 	struct io_mon mon;
-	struct io_src_pid pid_src;
+	struct my_pid_src s = {
+			.process_dead = false,
+	};
 	pid_t pid;
 	struct timeval timeout;
-	bool process_dead = 0;
-	void cb(struct io_src_pid *src_pid, pid_t local_pid, int status)
-	{
-		int wait_status;
-
-		CU_ASSERT(WIFEXITED(src_pid->status));
-		CU_ASSERT_EQUAL(WEXITSTATUS(src_pid->status), 0);
-		process_dead = true;
-		waitpid(local_pid, &wait_status, 0);
-		CU_ASSERT_EQUAL(status, wait_status);
-	};
 
 	/* normal use cases */
 	ret = io_mon_init(&mon);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
 	pid = launch("sleep", ".01", NULL);
 	CU_ASSERT_NOT_EQUAL_FATAL(pid, -1);
-	ret = io_src_pid_init(&pid_src, cb);
+	ret = io_src_pid_init(&s.pid_src, cb);
 	CU_ASSERT_EQUAL(ret, 0);
-	ret = io_src_pid_set_pid(&pid_src, pid);
+	ret = io_src_pid_set_pid(&s.pid_src, pid);
 	CU_ASSERT_EQUAL(ret, 0);
-	ret = io_mon_add_source(&mon, &(pid_src.src));
+	ret = io_mon_add_source(&mon, &(s.pid_src.src));
 	CU_ASSERT_EQUAL(ret, 0);
 
 	/* set the timer */
@@ -136,17 +146,17 @@ static void testSRC_PID_INIT(void)
 	if (ret < 0)
 		goto out;
 
-	CU_ASSERT(process_dead)
+	CU_ASSERT(s.process_dead);
 
 out :
 	/* cleanup */
 	io_mon_clean(&mon);
-	io_src_pid_clean(&pid_src);
+	io_src_pid_clean(&s.pid_src);
 
 	/* error use cases */
 	ret = io_src_pid_init(0, cb);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
-	ret = io_src_pid_init(&pid_src, NULL);
+	ret = io_src_pid_init(&s.pid_src, NULL);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
 }
 
@@ -155,31 +165,20 @@ static void testSRC_PID_SET_PID(void)
 	int ret;
 	fd_set rfds;
 	struct io_mon mon;
-	struct io_src_pid pid_src;
+	struct my_pid_src s;
 	pid_t pid;
 	struct timeval timeout;
-	bool process_dead = 0;
-	void cb(struct io_src_pid *src_pid, pid_t local_pid, int status)
-	{
-		int wait_status;
-
-		CU_ASSERT(WIFEXITED(status));
-		CU_ASSERT_EQUAL(WEXITSTATUS(status), 0);
-		process_dead = true;
-		waitpid(local_pid, &wait_status, 0);
-		CU_ASSERT_EQUAL(status, wait_status);
-	};
 
 	/* normal use cases */
 	ret = io_mon_init(&mon);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
 	pid = launch("sleep", ".01", NULL);
 	CU_ASSERT_NOT_EQUAL_FATAL(pid, -1);
-	ret = io_src_pid_init(&pid_src, cb);
+	ret = io_src_pid_init(&s.pid_src, cb);
 	CU_ASSERT_EQUAL(ret, 0);
-	ret = io_src_pid_set_pid(&pid_src, pid);
+	ret = io_src_pid_set_pid(&s.pid_src, pid);
 	CU_ASSERT_EQUAL(ret, 0);
-	ret = io_mon_add_source(&mon, &(pid_src.src));
+	ret = io_mon_add_source(&mon, &(s.pid_src.src));
 	CU_ASSERT_EQUAL(ret, 0);
 
 	/* set the timer */
@@ -206,13 +205,13 @@ static void testSRC_PID_SET_PID(void)
 	if (ret < 0)
 		goto out;
 
-	CU_ASSERT(process_dead)
+	CU_ASSERT(s.process_dead);
 
 	/* reuse the pid source */
-	process_dead = false;
+	s.process_dead = false;
 	pid = launch("sleep", ".01", NULL);
 	CU_ASSERT_NOT_EQUAL_FATAL(pid, -1);
-	ret = io_src_pid_set_pid(&pid_src, pid);
+	ret = io_src_pid_set_pid(&s.pid_src, pid);
 	CU_ASSERT_EQUAL(ret, 0);
 
 	/* set the timer */
@@ -239,19 +238,24 @@ static void testSRC_PID_SET_PID(void)
 	if (ret < 0)
 		goto out;
 
-	CU_ASSERT(process_dead)
+	CU_ASSERT(s.process_dead);
 
 out :
 	/* cleanup */
 	io_mon_clean(&mon);
-	io_src_pid_clean(&pid_src);
+	io_src_pid_clean(&s.pid_src);
 
 	/* error use cases */
 	/* fail if watching an already dead process */
-	ret = io_src_pid_set_pid(&pid_src, pid);
+	ret = io_src_pid_set_pid(&s.pid_src, pid);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
 	ret = io_src_pid_set_pid(NULL, 1);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
+}
+
+static void dummy_cb(struct io_src_pid *local_src, pid_t pid, int status)
+{
+
 }
 
 static void testSRC_PID_GET_SOURCE(void)
@@ -259,11 +263,6 @@ static void testSRC_PID_GET_SOURCE(void)
 	int ret;
 	struct io_src_pid pid_src;
 	struct io_src *src;
-
-	void dummy_cb(struct io_src_pid *local_src, pid_t pid, int status)
-	{
-
-	}
 
 	/* normal use cases */
 	ret = io_src_pid_init(&(pid_src), dummy_cb);
