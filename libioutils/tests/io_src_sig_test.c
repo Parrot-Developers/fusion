a473cf5767fdd537fc860461efe645caed7e2e02
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-20 15:29:08 +0100

    [DEV][io] removed nested functions from sig tests
    Change-Id: I53c098803838cad1fac2d22154b38072feae1227

--- libioutils/tests/io_src_sig_test.c
+++ libioutils/tests/io_src_sig_test.c
@@ -33,6 +33,36 @@ static int sigsets_are_equals(sigset_t *mask1, sigset_t *mask2)
 	return 0 == memcmp(mask1, mask2, sizeof(*mask1));
 }
 
+#define STATE_START 0
+#define STATE_SIGUSR1_RECEIVED 1
+#define STATE_SIGUSR2_RECEIVED 2
+#define STATE_ALL_DONE 3
+
+struct my_sig_src {
+	struct io_src_sig src_sig;
+	int state;
+	bool loop;
+
+};
+
+static void sig_cb(struct io_src_sig *sig, struct signalfd_siginfo *si)
+{
+	int ret;
+	struct my_sig_src *s = ut_container_of(sig, struct my_sig_src, src_sig);
+
+	if (SIGUSR1 == si->ssi_signo) {
+		CU_ASSERT(!(STATE_SIGUSR2_RECEIVED & s->state));
+		reached_state(&s->state, STATE_SIGUSR1_RECEIVED);
+		ret = kill(getpid(), SIGUSR2);
+	} else if (SIGUSR2 == si->ssi_signo) {
+		CU_ASSERT(STATE_SIGUSR1_RECEIVED & s->state);
+		reached_state(&s->state, STATE_SIGUSR2_RECEIVED);
+		s->loop = false;
+	} else {
+		CU_ASSERT(0);
+	}
+}
+
 /*
  * Main test. sends ourselves signals and check we receive them :
  *   * send / received a sigusr1
@@ -46,28 +76,12 @@ static void testSRC_SIG_INIT(void)
 	fd_set rfds;
 	int ret;
 	struct io_mon mon;
-	struct io_src_sig src_sig;
 	bool loop = true;
 	struct timeval timeout;
-#define STATE_START 0
-#define STATE_SIGUSR1_RECEIVED 1
-#define STATE_SIGUSR2_RECEIVED 2
-#define STATE_ALL_DONE 3
-	int state = STATE_START;
-	void sig_cb(struct io_src_sig *sig, struct signalfd_siginfo *si)
-	{
-		if (SIGUSR1 == si->ssi_signo) {
-			CU_ASSERT(!(STATE_SIGUSR2_RECEIVED & state));
-			reached_state(&state, STATE_SIGUSR1_RECEIVED);
-			ret = kill(getpid(), SIGUSR2);
-		} else if (SIGUSR2 == si->ssi_signo) {
-			CU_ASSERT(STATE_SIGUSR1_RECEIVED & state);
-			reached_state(&state, STATE_SIGUSR2_RECEIVED);
-			loop = false;
-		} else {
-			CU_ASSERT(0);
-		}
-	}
+	struct my_sig_src s = {
+			.state = STATE_START,
+			.loop = false,
+	};
 
 	ret = io_mon_init(&mon);
 	CU_ASSERT_EQUAL(ret, 0);
@@ -75,10 +89,10 @@ static void testSRC_SIG_INIT(void)
 	sigemptyset(&old_mask);
 	ret = sigprocmask(SIG_SETMASK, NULL, &old_mask);
 	CU_ASSERT_EQUAL(ret, 0);
-	ret = io_src_sig_init(&src_sig, sig_cb, SIGUSR1, SIGUSR2, NULL);
+	ret = io_src_sig_init(&s.src_sig, sig_cb, SIGUSR1, SIGUSR2, NULL);
 	CU_ASSERT_EQUAL_FATAL(ret, 0);
 
-	ret = io_mon_add_source(&mon, &(src_sig.src));
+	ret = io_mon_add_source(&mon, &(s.src_sig.src));
 	CU_ASSERT_EQUAL(ret, 0);
 
 	ret = kill(getpid(), SIGUSR1);
@@ -109,29 +123,29 @@ static void testSRC_SIG_INIT(void)
 		if (ret < 0)
 			goto out;
 
-		loop = STATE_ALL_DONE != state;
+		loop = STATE_ALL_DONE != s.state;
 	}
 
 out:
 	/* debriefing */
-	CU_ASSERT(state & STATE_SIGUSR1_RECEIVED);
-	CU_ASSERT(state & STATE_SIGUSR2_RECEIVED);
+	CU_ASSERT(s.state & STATE_SIGUSR1_RECEIVED);
+	CU_ASSERT(s.state & STATE_SIGUSR2_RECEIVED);
 
 	/* cleanup */
-	io_src_sig_clean(&src_sig);
+	io_src_sig_clean(&s.src_sig);
 	io_mon_clean(&mon);
 
 	/* error use cases */
 	ret = io_src_sig_init(NULL, sig_cb, SIGUSR1, SIGUSR2, NULL);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
-	ret = io_src_sig_init(&src_sig, NULL, SIGUSR1, SIGUSR2, NULL);
+	ret = io_src_sig_init(&s.src_sig, NULL, SIGUSR1, SIGUSR2, NULL);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
-	ret = io_src_sig_init(&src_sig, sig_cb, NULL);
+	ret = io_src_sig_init(&s.src_sig, sig_cb, NULL);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
 	/* neither SIGSTOP nor SIGKILL can be caught or ignored */
-	ret = io_src_sig_init(&src_sig, sig_cb, SIGSTOP, NULL);
+	ret = io_src_sig_init(&s.src_sig, sig_cb, SIGSTOP, NULL);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
-	ret = io_src_sig_init(&src_sig, sig_cb, SIGKILL, NULL);
+	ret = io_src_sig_init(&s.src_sig, sig_cb, SIGKILL, NULL);
 	CU_ASSERT_NOT_EQUAL(ret, 0);
 
 	/* check sigmask hasn't changed */
