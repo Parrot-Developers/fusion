ea3f69aa9fe3112ca58c3b7a47195ae7ee87e7fd
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-20 15:31:04 +0100

    [DEV][io] removed nested functions from src sep tests
    Change-Id: I4ac7c8ae0376db06933b544f9780dfbb63259491

--- libioutils/tests/io_src_sep_test.c
+++ libioutils/tests/io_src_sep_test.c
@@ -43,6 +43,7 @@ static const int sep_double[2] = {'\r', '\n'};
 struct my_sep_src {
 	struct io_src_sep src_sep;
 	int pipefds[2];
+	int state;
 };
 
 #define to_my_src_sep(p) rs_container_of(p, struct my_sep_src, src_sep)
@@ -62,44 +63,47 @@ static void my_sep_clean(struct my_sep_src *my_sep)
 	io_src_sep_clean(&(my_sep->src_sep));
 }
 
-/* main and only test. sends ourselves messages and check we receive them */
-static void testSRC_SEP(const int sep_pair[2], const char *big_msg, size_t sz)
-{
-	fd_set rfds;
-	int ret;
-	struct io_mon mon;
-	struct my_sep_src src_sep;
-	bool loop = true;
-	struct timeval timeout;
 #define STATE_START 0
 #define STATE_MSG1_RECEIVED 1
 #define STATE_MSG2_RECEIVED 2
 #define STATE_MSG3_RECEIVED 4
 #define STATE_TIMER_EXPIRED 8
 #define STATE_ALL_DONE 15
-	int state = STATE_START;
-	void sep_cb(struct io_src_sep *sep, char *chunk, unsigned len)
-	{
+static void sep_cb(struct io_src_sep *sep, char *chunk, unsigned len)
+{
+	struct my_sep_src *s = ut_container_of(sep, struct my_sep_src, src_sep);
 /*		printf("received %u byte(s) : \"%.*s\"\n", len, len, chunk); */
 
-		CU_ASSERT_NOT_EQUAL(state,
-				STATE_ALL_DONE & ~STATE_TIMER_EXPIRED);
-		CU_ASSERT_NOT_EQUAL(0, memcmp(chunk, MSG4, strlen(MSG4)));
+	CU_ASSERT_NOT_EQUAL(s->state,
+			STATE_ALL_DONE & ~STATE_TIMER_EXPIRED);
+	CU_ASSERT_NOT_EQUAL(0, memcmp(chunk, MSG4, strlen(MSG4)));
 
-		if (0 == memcmp(chunk, MSG1, strlen(MSG1))) {
-			CU_ASSERT_EQUAL(state, STATE_START);
-			reached_state(&state, STATE_MSG1_RECEIVED);
+	if (0 == memcmp(chunk, MSG1, strlen(MSG1))) {
+		CU_ASSERT_EQUAL(s->state, STATE_START);
+		reached_state(&s->state, STATE_MSG1_RECEIVED);
 
-			CU_ASSERT_EQUAL(len, strlen(MSG1) + 1 + sep->two_bytes);
-		} else if (0 == memcmp(chunk, MSG2, strlen(MSG2))) {
-			CU_ASSERT_EQUAL(state, STATE_MSG1_RECEIVED);
-			reached_state(&state, STATE_MSG2_RECEIVED);
-		} else if (0 == memcmp(chunk, MSG3, strlen(MSG3))) {
-			CU_ASSERT_EQUAL(state, STATE_MSG1_RECEIVED |
-					STATE_MSG2_RECEIVED);
-			reached_state(&state, STATE_MSG3_RECEIVED);
-		}
+		CU_ASSERT_EQUAL(len, strlen(MSG1) + 1 + sep->two_bytes);
+	} else if (0 == memcmp(chunk, MSG2, strlen(MSG2))) {
+		CU_ASSERT_EQUAL(s->state, STATE_MSG1_RECEIVED);
+		reached_state(&s->state, STATE_MSG2_RECEIVED);
+	} else if (0 == memcmp(chunk, MSG3, strlen(MSG3))) {
+		CU_ASSERT_EQUAL(s->state, STATE_MSG1_RECEIVED |
+				STATE_MSG2_RECEIVED);
+		reached_state(&s->state, STATE_MSG3_RECEIVED);
 	}
+}
+
+/* main and only test. sends ourselves messages and check we receive them */
+static void testSRC_SEP(const int sep_pair[2], const char *big_msg, size_t sz)
+{
+	fd_set rfds;
+	int ret;
+	struct io_mon mon;
+	struct my_sep_src src_sep = {
+			.state = STATE_START,
+	};
+	bool loop = true;
+	struct timeval timeout;
 
 	ret = pipe(src_sep.pipefds);
 	CU_ASSERT_EQUAL(ret, 0);
@@ -133,8 +137,8 @@ static void testSRC_SEP(const int sep_pair[2], const char *big_msg, size_t sz)
 
 		/* timeout, normal */
 		if (0 == ret) {
-			CU_ASSERT_NOT_EQUAL(state, STATE_ALL_DONE);
-			reached_state(&state, STATE_TIMER_EXPIRED);
+			CU_ASSERT_NOT_EQUAL(src_sep.state, STATE_ALL_DONE);
+			reached_state(&src_sep.state, STATE_TIMER_EXPIRED);
 		} else {
 			ret = io_mon_process_events(&mon);
 			CU_ASSERT(ret >= 0);
@@ -142,15 +146,15 @@ static void testSRC_SEP(const int sep_pair[2], const char *big_msg, size_t sz)
 				goto out;
 		}
 
-		loop = STATE_ALL_DONE != state;
+		loop = STATE_ALL_DONE != src_sep.state;
 	}
 
 out:
 	/* debriefing */
-	CU_ASSERT(state & STATE_MSG1_RECEIVED);
-	CU_ASSERT(state & STATE_MSG2_RECEIVED);
-	CU_ASSERT(state & STATE_MSG3_RECEIVED);
-	CU_ASSERT(state & STATE_TIMER_EXPIRED);
+	CU_ASSERT(src_sep.state & STATE_MSG1_RECEIVED);
+	CU_ASSERT(src_sep.state & STATE_MSG2_RECEIVED);
+	CU_ASSERT(src_sep.state & STATE_MSG3_RECEIVED);
+	CU_ASSERT(src_sep.state & STATE_TIMER_EXPIRED);
 
 	/* error cases */
 	ret = io_src_sep_init(NULL, src_sep.pipefds[0], sep_cb, sep_pair[0],
@@ -181,17 +185,17 @@ static void testSRC_SEP_INIT(void)
 	testSRC_SEP(sep_double, big_msg_double, strlen(big_msg_double));
 }
 
+static void dummy_cb(struct io_src_sep *sep, char *chunk, unsigned len)
+{
+
+}
+
 static void testSRC_SEP_GET_SOURCE(void)
 {
 	int ret;
 	struct io_src_sep sep_src;
 	struct io_src *src;
 	int pipefd[2] = {-1, -1};
-
-	void dummy_cb(struct io_src_sep *sep, char *chunk, unsigned len)
-	{
-
-	}
 
 	ret = pipe(pipefd);
 	CU_ASSERT_NOT_EQUAL_FATAL(ret, -1);
