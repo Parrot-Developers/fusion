327507541e2c993b2ed67598af72a8b97515087d
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-01-14 09:20:20 +0100

    [DEV][io] thread support implementation
    Change-Id: I115d08efa0533749d4b8c513bdea90461c4a0366

---
+++ libioutils/include/io_src_thread.h
@@ -0,0 +1,105 @@
+/**
+ * @file io_src_thread.h
+ * @date 15 jan. 2016
+ * @author nicolas.carrier@parrot.com
+ * @brief Source for watching for monitoring a thread
+ *
+ * Copyright (C) 2016 Parrot S.A.
+ */
+#ifndef IO_SRC_THREAD_H_
+#define IO_SRC_THREAD_H_
+#include <pthread.h>
+
+#include <stdbool.h>
+
+#include <io_src.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @struct io_src_thread
+ * @brief thread source type
+ */
+struct io_src_thread;
+
+/**
+ * @typedef io_src_thread_start_routine
+ * @brief main function of the thread, whose termination will be notified by the
+ * termination callback
+ * @param thread_src source representing the thread
+ * @return this value will be passed as the ret argument of the termination
+ * callback, when the thread terminates
+ */
+typedef int (io_src_thread_start_routine)(struct io_src_thread *thread_src);
+
+/**
+ * @typedef io_src_thread_termination_cb
+ * @brief callback called when a thread has terminated
+ * @param thread_src source representing the thread
+ * @param ret return value of the thread's start routine
+ */
+typedef void (io_src_thread_termination_cb)(struct io_src_thread *thread_src,
+		int ret);
+
+struct io_src_thread {
+	/** inner monitor source */
+	struct io_src src;
+	/** the very thread */
+	pthread_t thread;
+	/** true iif the pthread_create call has succeeded */
+	bool thread_initialized;
+	/** communication pipe, notifying of the thread's death */
+	int pipefd[2];
+	/** main function of the thread */
+	io_src_thread_start_routine *start_routine;
+	/** callback called when the thread terminates */
+	io_src_thread_termination_cb *termination_cb;
+	/** storage for the start routine's return value */
+	int ret;
+};
+
+/**
+ * Initializes a thread source.
+ * @param thread_src Thread source to initialize
+ * @return errno compatible value
+ */
+int io_src_thread_init(struct io_src_thread *thread_src);
+
+/**
+ * Starts the thread, by calling it's start_routine
+ * @param thread_src thread source representing the thread to start
+ * @param attr optional pthread attributes to configure the thread with
+ * @param start_routine main function of the thread
+ * @param termination_cb callback called when the thread terminates
+ * @return errno-compatible negative value on error
+ */
+int io_src_thread_start(struct io_src_thread *thread_src,
+		const pthread_attr_t *attr,
+		io_src_thread_start_routine *start_routine,
+		io_src_thread_termination_cb *termination_cb);
+
+/**
+ * Retrieves the underlying io_src of the thread
+ * @param thread_src I/O source representing the thread
+ * @return io_src of the thread
+ */
+static inline struct io_src *io_src_thread_get_source(
+		struct io_src_thread *thread_src)
+{
+	return NULL == thread_src ? NULL : &thread_src->src;
+}
+
+/**
+ * Cleans the io_src_thread, if the thread hasn't terminated yet, it is
+ * canceled.
+ * @param thread_src Thread source to initialize
+ */
+void io_src_thread_clean(struct io_src_thread *thread_src);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IO_SRC_THREAD_H_ */
---
+++ libioutils/src/io_src_thread.c
@@ -0,0 +1,111 @@
+/**
+ * @file io_src_thread.c
+ * @date 15 jan. 2016
+ * @author nicolas.carrier@parrot.com
+ * @brief Source for watching for monitoring a thread
+ *
+ * Copyright (C) 2016 Parrot S.A.
+ */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif /* _GNU_SOURCE */
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <errno.h>
+#include <string.h>
+
+#include <io_src_thread.h>
+
+#include <ut_file.h>
+
+/**
+ * Main function of the thead, executes the user-provided start routine, then
+ * notifies via the thread's file descriptor
+ * @param arg thread source
+ */
+static void *thread_src_start_routine(void *arg)
+{
+	char *byte = 0;
+	ssize_t sret;
+	struct io_src_thread *thread_src = arg;
+
+	thread_src->ret = thread_src->start_routine(thread_src);
+
+	sret = write(thread_src->pipefd[1], &byte, sizeof(byte));
+	/* sret is discarded */
+
+	return &thread_src->ret;
+}
+
+static void thread_src_cb(struct io_src *src)
+{
+	union {
+		void *v;
+		int *i;
+	} ret;
+	ssize_t sret;
+	char byte;
+	struct io_src_thread *thread_src = ut_container_of(src,
+			struct io_src_thread, src);
+
+	sret = read(thread_src->pipefd[0], &byte, sizeof(byte));
+	if (sret == -1)
+		byte = -errno;
+
+	pthread_join(thread_src->thread, &ret.v);
+	thread_src->termination_cb(thread_src, *ret.i);
+}
+
+int io_src_thread_init(struct io_src_thread *thread_src)
+{
+	int ret;
+
+	if (thread_src == NULL)
+		return -EINVAL;
+
+	memset(thread_src, 0, sizeof(*thread_src));
+	ret = pipe2(thread_src->pipefd, O_CLOEXEC | O_NONBLOCK);
+	if (ret == -1)
+		return -errno;
+
+	return io_src_init(&thread_src->src, thread_src->pipefd[0], IO_IN,
+			thread_src_cb);
+}
+
+int io_src_thread_start(struct io_src_thread *thread_src,
+		const pthread_attr_t *attr,
+		io_src_thread_start_routine *start_routine,
+		io_src_thread_termination_cb *termination_cb)
+{
+	int ret;
+
+	if (thread_src == NULL || start_routine == NULL ||
+			termination_cb == NULL)
+		return -EINVAL;
+
+	thread_src->start_routine = start_routine;
+	thread_src->termination_cb = termination_cb;
+	ret = pthread_create(&thread_src->thread, attr,
+			thread_src_start_routine, thread_src);
+	if (ret != 0)
+		return -ret;
+	thread_src->thread_initialized = true;
+
+	return 0;
+}
+
+void io_src_thread_clean(struct io_src_thread *thread_src)
+{
+	io_src_clean(&thread_src->src);
+	if (thread_src->thread_initialized) {
+		pthread_cancel(thread_src->thread);
+		pthread_join(thread_src->thread, NULL);
+	}
+	thread_src->thread_initialized = false;
+	ut_file_fd_close(thread_src->pipefd + 0);
+	ut_file_fd_close(thread_src->pipefd + 1);
+	thread_src->ret = 0;
+	thread_src->start_routine = NULL;
+	thread_src->termination_cb = NULL;
+}
