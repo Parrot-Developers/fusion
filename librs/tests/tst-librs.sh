5c5e04a09d3d5e83fd6740f617e6d76fe8ca644c
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-10-20 13:27:50 +0200

    [DEV][rs] compliance with the new automated tests infrastructure
    Change-Id: I2f40fb4a883b229cefbaf0073feed743250dbc3f

---
+++ librs/tests/tst-librs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+module_name=librs
+fautes xml ${module_name}.so
+
+if [ -n "${CUNIT_OUT_NAME}" ]; then
+	mv ${module_name}-Results.xml ${CUNIT_OUT_NAME}-Results.xml
+	mv ${module_name}-Listing.xml ${CUNIT_OUT_NAME}-Listing.xml
+fi
