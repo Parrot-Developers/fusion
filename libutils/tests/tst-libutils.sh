36ea4f5ef4b6adef828580f2517f23b77fad6d36
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-10-20 11:15:08 +0200

    [DEV][ut] go back to using the fautes wrapper
    as the tests aren't executed in a chroot environment or on target,
    we can't use the absolute path to the library
    
    Change-Id: Ia2341aefd67f84c5f8e1c2776f14049a67bab255

--- libutils/tests/tst-libutils.sh
+++ libutils/tests/tst-libutils.sh
@@ -3,7 +3,7 @@
 set -e
 
 module_name=libutils
-FAUTES_XML=y /usr/lib/${module_name}.so
+fautes xml ${module_name}.so
 
 if [ -n "${CUNIT_OUT_NAME}" ]; then
 	mv ${module_name}-Results.xml ${CUNIT_OUT_NAME}-Results.xml
